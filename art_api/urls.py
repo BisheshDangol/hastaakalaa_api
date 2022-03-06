dfa006c125b9b762592686d7651738d13ddf508b
BisheshDangol <bishesh01dangol@gmail.com>
2022-03-06 10:21:11 +0545

    Adding url in art_api folder

--- art_api/urls.py
+++ art_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import ArtDetail, CreateArt, ListUserArtPost, PostListDetailFilter
+from .views import ArtDetail, CreateArt, ListUserArtPost, PostListDetailFilter, RetrieveAllArtPost
 
 app_name = 'art_api'
 
@@ -15,4 +15,5 @@ urlpatterns = [
     path('detail_art/<str:pk>/', ArtDetail.as_view(), name='detailart'),
     path('search/custom/', PostListDetailFilter.as_view(), name='searchart'),
     path('get_all_art_post_user/', ListUserArtPost.as_view(), name='listuserartposts'),
+    path('retrieve_art_post/', RetrieveAllArtPost.as_view(), name='retrieveartpost'),
 ]
